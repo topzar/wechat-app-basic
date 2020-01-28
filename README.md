1498aad7b3cb694191c75b833075d7baea96f9c4
oyghan <742163033@qq.com>
2020-01-29 00:58:17 +0800

    feat: 新增小程序版本更新功能

---
+++ README.md
@@ -0,0 +1,3 @@
+## 开箱即可用的微信小程序原生开发框架
+
+- [x] 微信小程序版本管理功能。<a href="https://developers.weixin.qq.com/miniprogram/dev/api/base/update/UpdateManager.html" target="_blank">官方文档连接</a>
--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    //UpdateManager对象，用来管理更新，可通过 wx.getUpdateManager接口获取实例
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调,返回true或false
+      console.log(res.hasUpdate)
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    //监听小程序有版本更新事件
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
         }
-      }
+      })
     })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
+    
   },
   globalData: {
     userInfo: null
